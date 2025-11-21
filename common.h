4c8ed0ea7918d275c6c834ca08a9372f74984810
wonderwhy110 <kurletova@gmail.com>
2025-11-21 14:02:38 +0300

    Create common.h

---
+++ common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdint.h>
+
+uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod);
+bool ConvertStringToUI64(const char *str, uint64_t *val);
+
+#endif
