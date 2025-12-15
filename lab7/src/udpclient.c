e8c5ccc2f4d3b43c457f07d0598cdddd89eaedbd
wonderwhy110 <kurletova@gmail.com>
2025-12-15 13:41:42 +0300

    Update udpclient.c

--- lab7/src/udpclient.c
+++ lab7/src/udpclient.c
@@ -1,7 +1,6 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <arpa/inet.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -9,30 +8,31 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 20001
-#define BUFSIZE 1024
 #define SADDR struct sockaddr
-#define SLEN sizeof(struct sockaddr_in)
 
 int main(int argc, char **argv) {
-  int sockfd, n;
-  char sendline[BUFSIZE], recvline[BUFSIZE + 1];
-  struct sockaddr_in servaddr;
-  struct sockaddr_in cliaddr;
-
-  if (argc != 2) {
-    printf("usage: client <IPaddress of server>\n");
+  if (argc != 4) {
+    printf("Usage: %s <ip> <port> <buffer_size>\n", argv[0]);
     exit(1);
   }
+
+  char *ip = argv[1];
+  int port = atoi(argv[2]);
+  int bufsize = atoi(argv[3]);
+  
+  int sockfd, n;
+  char sendline[bufsize], recvline[bufsize + 1];
+  struct sockaddr_in servaddr;
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(SERV_PORT);
+  servaddr.sin_port = htons(port);
 
-  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 0) {
+  if (inet_pton(AF_INET, ip, &servaddr.sin_addr) < 0) {
     perror("inet_pton problem");
     exit(1);
   }
+  
   if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
     perror("socket problem");
     exit(1);
@@ -40,13 +40,13 @@ int main(int argc, char **argv) {
 
   write(1, "Enter string\n", 13);
 
-  while ((n = read(0, sendline, BUFSIZE)) > 0) {
-    if (sendto(sockfd, sendline, n, 0, (SADDR *)&servaddr, SLEN) == -1) {
+  while ((n = read(0, sendline, bufsize)) > 0) {
+    if (sendto(sockfd, sendline, n, 0, (SADDR *)&servaddr, sizeof(servaddr)) == -1) {
       perror("sendto problem");
       exit(1);
     }
 
-    if (recvfrom(sockfd, recvline, BUFSIZE, 0, NULL, NULL) == -1) {
+    if (recvfrom(sockfd, recvline, bufsize, 0, NULL, NULL) == -1) {
       perror("recvfrom problem");
       exit(1);
     }
